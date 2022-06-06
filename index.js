2b0a0b7c1798dfe3321578a570ac8cd297e312e5
Promise.All <2238480780@qq.com>
2022-06-06 14:46:04 +0800

    10

--- index.js
+++ index.js
@@ -78,14 +78,14 @@ function init() {
     accessSync(`${process.cwd()}/exeBuild.config.js`)
     console.log(`Build Config File: ${yellow('exeBuild.config.js')}`);
   } catch (err) {
-    copyFileSync('./exeBuild.config.js', `${process.cwd()}/exeBuild.config.js`)
+    copyFileSync(`${__dirname}/exeBuild.config.js`, `${process.cwd()}/exeBuild.config.js`)
     console.log(`New Build Config File: ${yellow('exeBuild.config.js')}`);
   }
   try {
     accessSync(`${process.cwd()}/app.ico`)
     console.log(`Icon File: ${yellow('app.ico')}`);
   } catch (err) {
-    copyFileSync('./app.ico', `${process.cwd()}/app.ico`)
+    copyFileSync(`${__dirname}/app.ico`, `${process.cwd()}/app.ico`)
     console.log(`New Icon File: ${yellow('app.ico')}`);
   }
 }
